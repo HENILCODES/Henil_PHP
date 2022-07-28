eb7f894a4e0b080cef6bb5c57f393620e541bc92
HENILCODES <prajapatihenil1112@gmail.com>
2022-07-28 18:56:32 +0530

    create sepret log in page

---
+++ A Web/user/File/db.php
@@ -0,0 +1,5 @@
+<?php
+// $Db_con = mysqli_connect("sql300.epizy.com", "epiz_32115700", "ykadE1TpC3","epiz_32115700_Users");
+
+$Db_con = mysqli_connect("localhost", "root", "", "henilcodeSite");
+?>
