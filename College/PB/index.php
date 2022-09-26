5ce984882f8bdbe55b4142d1c47dd72a303336ff
HENILCODES <prajapatihenil1112@gmail.com>
2022-09-26 12:47:10 +0530

    add PB

---
+++ College/PB/index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link rel="stylesheet" href="CSS/bootstrap.min.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <ul class="nav nav-tabs">
+        <li class="nav-item">
+          <a href="" class="nav-link active"> Home</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link "> Home</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link "> Home</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link "> Home</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <div class="container my-3">
+    s
+  </div>
+</body>
+<script src="JS/bootstrap.min.js"></script>
+<script src="JS/popper.min.js"></script>
+
+</html>
