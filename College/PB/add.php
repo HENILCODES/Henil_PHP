45d27d875f0053df9f43c84d479cf4c0bad1dfaf
HENILCODES <prajapatihenil1112@gmail.com>
2022-09-28 08:15:35 +0530

    add php in watch store

---
+++ College/PB/add.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Henil code</title>
+    <link rel="stylesheet" href="CSS/bootstrap.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg bg-light shadow-sm">
+        <div class="container-fluid">
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <a href="index.php" class="nav-link"> Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="add.php" class="nav-link active"> add </a>
+                </li>
+                <li class="nav-item">
+                    <a href="" class="nav-link "> delete</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container my-3">
+        <form class="row g-3 w-50 m-auto" autocomplete="off" method="post" enctype="multipart/form-data">
+            <div class="input-group">
+                <spna class="input-group-text w-25 justify-content-center">#</spna>
+                <input required type="text" class="form-control" name="PID" placeholder="Product ID">
+            </div>
+            <div class="input-group">
+                <span class="input-group-text w-25 justify-content-center">Name</span>
+                <input required type="text" class="form-control" name="PNAME" placeholder="Name">
+            </div>
+            <div class="input-group">
+                <span class="input-group-text w-25 justify-content-center">$</span>
+                <input required type="text" class="form-control" name="PPRICE" placeholder="Price">
+            </div>
+            <div class="input-group">
+                <input type="file" class="form-control form-control-lg" name="PFILE" accept="image/*">
+            </div>
+            <div class="input-group">
+                <span class="input-group-text w-25 justify-content-center">Description</span>
+                <textarea class="form-control" placeholder="Type hear" name="PDESCRIP"></textarea>
+            </div>
+            <button class="btn btn-primary" name="UPLOAD">Submit</button>
+        </form>
+    </div>
+
+</body>
+<script src="JS/bootstrap.min.js"></script>
+<script src="JS/popper.min.js"></script>
+<?php
+include "db.php";
+
+if (isset($_REQUEST['UPLOAD'])) {
+    $PID = $_REQUEST['PID'];
+    $PNAME = $_REQUEST['PNAME'];
+    $PPRICE = $_REQUEST['PPRICE'];
+    $PFILE = $_FILES['PFILE']['name'];
+    $PDESCRIP = $_REQUEST['PDESCRIP'];
+
+    if (move_uploaded_file($_FILES['PFILE']['tmp_name'], "File/" . $PFILE)) {
+        $inse = "insert into watch_store (PID, PNAME, PRICE, IMAGE, DESCRIPTION) values ($PID,'$PNAME',$PPRICE,'$PFILE','$PDESCRIP')";
+        $re = mysqli_query($conn, $inse);
+        if ($re) {
+            echo "Add";
+        }
+    } else {
+        echo "eroor";
+    }
+}
+?>
+
+</html>
--- College/PB/index.php
+++ College/PB/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap demo</title>
+  <title>Henil code</title>
   <link rel="stylesheet" href="CSS/bootstrap.min.css">
 </head>
 
@@ -13,22 +13,33 @@
     <div class="container-fluid">
       <ul class="nav nav-tabs">
         <li class="nav-item">
-          <a href="" class="nav-link active"> Home</a>
+          <a href="index.php" class="nav-link active"> Home</a>
         </li>
         <li class="nav-item">
-          <a href="" class="nav-link "> Home</a>
+          <a href="add.php" class="nav-link "> add </a>
         </li>
         <li class="nav-item">
-          <a href="" class="nav-link "> Home</a>
-        </li>
-        <li class="nav-item">
-          <a href="" class="nav-link "> Home</a>
+          <a href="" class="nav-link "> delete</a>
         </li>
       </ul>
     </div>
   </nav>
   <div class="container my-3">
-    s
+    
+    <div class="card border border-dark" style="width:250px;">
+      <img src="IMG/audio.jpg" class="card-img-top" >
+      <div class="card-body">
+        <span class="badge text-bg-info">#2000</span>
+        <div class="d-flex justify-content-between">
+          <h5>Watch</h5>
+          <h5 class="card-title">2000</h5>
+        </div>
+        <div class="overflow-hidden opacity-50" style="height: 57px;">
+          <p class="card-text">Lorem ipsum dolor, sitnsgdj njasgd jh dg hdgf hf h sd kns df sdn fmsn dfnsv dfns ddsbvdfnb  sd gs jdg hgf jshd jsd fsd sh jfh df sdfshjgf jdsj fdj amet consectetur adipisicing elit. Molestias nisi nobis dolores. </p>
+        </div>
+        <button class="btn btn-primary">Buy</button>
+      </div>
+    </div>
   </div>
 </body>
 <script src="JS/bootstrap.min.js"></script>
