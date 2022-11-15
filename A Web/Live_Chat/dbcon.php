510ea2fb05b6a0fbb138f4f29f9a5239231ae659
henilcodes <prajapatihenil1112@gmail.com>
2022-11-15 07:50:52 +0530

    edit some

--- A Web/Live_Chat/dbcon.php
+++
@@ -1,5 +0,0 @@
-<?php
-// $conn = mysqli_connect("sql300.epizy.com", "epiz_32115700", "ykadE1TpC3","epiz_32115700_Chat");
-
-$conn = mysqli_connect("localhost", "root", "", "live_chat");
-?>
--- A Web/Music/PHP/sec.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (!$_SESSION['AUD_User']) {
-    header("location: UserLog.html");
-}
-?>
